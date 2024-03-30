687a173298c40e82304d6401db673d576df4c730
sad <165470922+wint18390@users.noreply.github.com>
2024-03-30 21:21:36 +0800

    Update index.php
    Signed-off-by: sad <165470922+wint18390@users.noreply.github.com>

--- index.php
+++ index.php
@@ -3,6 +3,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4818230065245501"
+     crossorigin="anonymous"></script>
 </head>
 <body>
     <h1>sad</h1>
