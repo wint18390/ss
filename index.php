826fd9f09f01f7b7b24fabacb7067efdd42cd456
sad <165470922+wint18390@users.noreply.github.com>
2024-03-30 21:26:51 +0800

    Update index.php
    Signed-off-by: sad <165470922+wint18390@users.noreply.github.com>

--- index.php
+++ index.php
@@ -5,6 +5,8 @@
     <title>Document</title>
     <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4818230065245501"
      crossorigin="anonymous"></script>
+    <meta name="google-adsense-account" content="ca-pub-4818230065245501">
+
 </head>
 <body>
     <h1>sad</h1>
